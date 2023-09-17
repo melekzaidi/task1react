37bc6a68b75586167f282666cf44058941121b25
melekzaidi <79268865+melekzaidi@users.noreply.github.com>
2023-09-17 21:44:02 +0000

    9 17 2023

---
+++ src/components/Header.js/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import logo from "./investment-calculator-logo.png"
+import  "./Header.css";
+export default function Header() {
+  return (
+    <header className="header">
+    <img src={logo} alt="logo" />
+    <h1>Investment Calculator</h1>
+  </header>
+  )
+}
