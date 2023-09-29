2c9ed1ef3ceb6a49f669db13614c3bb3fd38acdd
melek zaidi <melekzaidimk@gmail.com>
2023-09-30 00:14:55 +0100

    30-9-2023

--- src/components/UserInput/UserInput.js
+++ src/components/UserInput/UserInput.js
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./UserInput.css"
 export default function UserInput() {
+  const initialinput={'current-savings':1000,'yearly-contribution':1200,'expected-return':7,'duration':10}
+  const [userinput,setuserinput]=  useState(initialinput)
+  const submitHandler=(e)=>{
+    e.preventDefault();
+  }
+  const resetHandler=(e)=>{
+    e.preventDefault();
+  }
+  const inputchaneHandler=(input,value)=>{
+setuserinput((prevInput)=>{return{...prevInput,[input]:value}})
+  }
   return (
     <div>
-       <form className="form">
+       <form className="form" onSubmit={submitHandler}>
         <div className="input-group">
           <p>
             <label htmlFor="current-savings">Current Savings ($)</label>
-            <input type="number" id="current-savings" />
+            <input value={userinput['current-savings']} onChange={(event)=>inputchaneHandler('current-savings',event.target.value)} type="number" id="current-savings" />
           </p>
           <p>
             <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
-            <input type="number" id="yearly-contribution" />
+            <input value={userinput['yearly-contribution']} type="number" id="yearly-contribution" onChange={(event)=>inputchaneHandler('yearly-contribution',event.target.value)}  />
           </p>
         </div>
         <div className="input-group">
@@ -19,15 +30,15 @@ export default function UserInput() {
             <label htmlFor="expected-return">
               Expected Interest (%, per year)
             </label>
-            <input type="number" id="expected-return" />
+            <input value={userinput['expected-return']} type="number" id="expected-return" onChange={(event)=>inputchaneHandler('expected-return',event.target.value)} />
           </p>
           <p>
             <label htmlFor="duration">Investment Duration (years)</label>
-            <input type="number" id="duration" />
+            <input type="number" id="duration" value={userinput['duration']} onChange={(event)=>inputchaneHandler('duration',event.target.value)} />
           </p>
         </div>
         <p className="actions">
-          <button type="reset" className="buttonAlt">
+          <button type="reset" className="buttonAlt" onClick={resetHandler}>
             Reset
           </button>
           <button type="submit" className="button">
